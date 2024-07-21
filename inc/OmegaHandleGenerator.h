343d49c3be118a99d09247e728ff510571c54351
Omegaki113r <44817852+Omegaki113r@users.noreply.github.com>
2024-07-22 00:45:08 +0530

    initial commit with generator

---
+++ inc/OmegaHandleGenerator.h
@@ -0,0 +1,39 @@
+/**
+ * @file OmegaHandleGenerator.h
+ * @author Omegaki113r
+ * @date Sunday, 21st July 2024 6:24:30 pm
+ * @copyright Copyright 2024 - 2024 0m3g4ki113r, Xtronic
+ * */
+/*
+ * Project: OmegaHandleGenerator
+ * File Name: OmegaHandleGenerator.h
+ * File Created: Sunday, 21st July 2024 6:24:30 pm
+ * Author: Omegaki113r (omegaki113r@gmail.com)
+ * -----
+ * Last Modified: Sunday, 21st July 2024 6:38:23 pm
+ * Modified By: Omegaki113r (omegaki113r@gmail.com)
+ * -----
+ * Copyright 2024 - 2024 0m3g4ki113r, Xtronic
+ * -----
+ * HISTORY:
+ * Date      	By	Comments
+ * ----------	---	---------------------------------------------------------
+ */
+
+#ifndef __OMEGA_HANDLE_GENERATOR_H__
+#define __OMEGA_HANDLE_GENERATOR_H__
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#include <stdint.h>
+
+    uint64_t OmegaHandleGenerator_create_handle();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ src/OmegaHandleGenerator.c
@@ -0,0 +1,69 @@
+/**
+ * @file OmegaHandleGenerator.c
+ * @author Omegaki113r
+ * @date Sunday, 21st July 2024 6:28:31 pm
+ * @copyright Copyright 2024 - 2024 0m3g4ki113r, Xtronic
+ * */
+/*
+ * Project: OmegaHandleGenerator
+ * File Name: OmegaHandleGenerator.c
+ * File Created: Sunday, 21st July 2024 6:28:31 pm
+ * Author: Omegaki113r (omegaki113r@gmail.com)
+ * -----
+ * Last Modified: Monday, 22nd July 2024 12:41:03 am
+ * Modified By: Omegaki113r (omegaki113r@gmail.com)
+ * -----
+ * Copyright 2024 - 2024 0m3g4ki113r, Xtronic
+ * -----
+ * HISTORY:
+ * Date      	By	Comments
+ * ----------	---	---------------------------------------------------------
+ */
+
+#include "OmegaHandleGenerator.h"
+#include "OmegaBaseLoggingController.h"
+#include "OmegaUtilityDriver.h"
+
+#if __has_include(<sdkconfig.h>)
+#include <sdkconfig.h>
+#endif
+
+#define MAX_ITERATION_AMOUNT 100
+
+#define RAND() (rand() & 0x7fff)
+
+#define i_TYPE Vector, u64
+#include <vec.h>
+
+static Vector handles;
+
+uint64_t OmegaHandleGenerator_create_handle()
+{
+    u64 iteration_count = 0;
+    u64 generated_handle = 0;
+    do
+    {
+    regenerate_handle:
+        generated_handle = ((u64)RAND() << 48) ^ ((u64)RAND() << 35) ^ ((u64)RAND() << 22) ^
+                           ((u64)RAND() << 9) ^ ((u64)RAND() >> 4);
+        c_foreach(handle_iterator, Vector, handles)
+        {
+            if (generated_handle == *handle_iterator.ref)
+                goto regenerate_handle;
+        }
+    } while (generated_handle <= 0 && iteration_count++ < MAX_ITERATION_AMOUNT);
+    if (iteration_count >= MAX_ITERATION_AMOUNT)
+    {
+        OMEGA_LOGE("Unable to create valid handle");
+        generated_handle = 0;
+        goto response;
+    }
+    Vector_value *inserted_handle = Vector_push_back(&handles, generated_handle);
+    if (inserted_handle == NULL)
+    {
+        OMEGA_LOGE("Inserting handle to vector failed");
+        generated_handle = 0;
+    }
+response:
+    return generated_handle;
+}
